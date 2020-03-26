7b64fe3b2a3c6740c49cf910d6f86833ed4ab26c
ibrahiemhss <kekoreko3@gmail.com>
2020-03-26 10:55:04 +0200

    update

--- app/src/main/java/com/hijozaty/smsservice/Constants.java
+++ app/src/main/java/com/hijozaty/smsservice/Constants.java
@@ -4,7 +4,7 @@ package com.hijozaty.smsservice;
  * Created by hijozaty team 9/3/2020.
  */
 public class Constants {
-    public static final String SERVER_URL ="https://www.hijozaty.com/chat:3000";
+    public static final String SERVER_URL ="https://hijozaty.com/chat";
     //public static final String SERVER_URL ="http://www.hijozaty.com:3000";
 
     public static final String EXTR_APHONE ="phone_value";
